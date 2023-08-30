245c17ecd58b1926715bd9db577c3cd6a73fc746
Rein-PY <72543025+Rein-PY@users.noreply.github.com>
2023-08-30 14:30:02 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update $$ apt install -y curl wget git
-wget https://github.com/Rein-PY/HA_auto_install/raw/main/Start_config_HA_auto.tar.gz
+wget https://disk.yandex.ru/d/aPAiVBX6PfpFlg
 tar -xzvf Start_config_HA_auto.tar.gz
 ./home-assistant/zapusk.sh
 
