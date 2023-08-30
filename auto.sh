dde055cef969cc9164bbf4bc0adfb03e517d3996
Rein-PY <72543025+Rein-PY@users.noreply.github.com>
2023-08-30 15:53:16 +0300

    Update auto.sh

--- auto.sh
+++ auto.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+mkdir /mnt/data 
 wget https://disk.yandex.ru/d/aPAiVBX6PfpFlg
-tar -xzvf Start_config_HA_auto.tar.gz
+tar -xzvf Start_config_HA_auto.tar.gz /mnt/data/
 ./home-assistant/zapusk.sh
 
